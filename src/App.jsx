b2950a2b6b51f3151dfca89cd60c0617a0ad0990
Alonso <valenzuela.alvarez.a.a@gmail.com>
2025-05-07 18:07:11 -0400

    Lintear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`
 
 export function App () {
-    const [fact, setFact] = useState('lorem ipsum cat fact whatever')
-    const [imageURL, setImageURL] = useState()
+  const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+  const [imageURL, setImageURL] = useState()
 
-    useEffect(() =>{
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
-            .then(data => {
-                const {fact} = data
-                setFact(fact)
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
 
-                const firstWord = fact.split(' ',3).join(' ')
-                console.log(firstWord)
-                
-                fetch(`https://cataas.com/cat/says/${firstWord}`)
-                    .then(response =>{
-                        const {url} = response
-                        setImageURL(url)
-                    })
-            })
-    }, [])
+        const firstWord = fact.split(' ', 3).join(' ')
+        console.log(firstWord)
 
-    return(
-        <main>
-            <h1>App de gatitos</h1>
-            {fact && <p>{fact}</p>}
-            {imageURL && <img src={imageURL} alt="image that says the first word with a cat"></img>}
-        </main>
-    )
+        fetch(`https://cataas.com/cat/says/${firstWord}`)
+          .then(response => {
+            const { url } = response
+            setImageURL(url)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      {fact && <p>{fact}</p>}
+      {imageURL && <img src={imageURL} alt='image that says the first word with a cat' />}
+    </main>
+  )
 }
